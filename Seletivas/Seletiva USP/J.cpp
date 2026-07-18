25be842708e03a1bfe6552e6babbc5fb2974da17
vivisyama <vivisyama@usp.br>
2026-07-18 09:48:28 -0300

    Create J.cpp

---
+++ Seletivas/Seletiva USP/J.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+ 
+int main(){
+    
+    int n = 0, i = 1;
+ 
+    cin >> n;
+ 
+    cout << n << endl;
+ 
+    while(i != n+1){
+        
+        if(i == n){
+            
+            cout << 1 << endl; 
+ 
+        }else{
+ 
+            cout << 1 << " ";
+ 
+        }
+        
+        i++;
+ 
+    }
+    
+}
